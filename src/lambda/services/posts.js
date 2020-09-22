8b75bd3a741ef0c1e743493d9bea89c7f53aca90
Nicolás Delgado Abad <nicolas.delgado@altia.es>
2020-09-22 16:39:02 +0200

    first

---
+++ src/lambda/services/posts.js
@@ -0,0 +1,52 @@
+const fetch = require("node-fetch");
+const { imageDataFromResponse, maxPostPage } = require("../utils/transform");
+
+const FIRST_ITERABLE_PAGE = 2;
+
+const postsFromUrl = (url, from = true) => {
+  return fetch(url)
+    .then((response) => response.text())
+    .then((response) => {
+      return imageDataFromResponse(response, from);
+    });
+};
+
+const firstPageDataFromUrl = (url, from = true) => {
+  return fetch(url)
+    .then((response) => response.text())
+    .then((response) => {
+      return {
+        firstPosts: imageDataFromResponse(response, from),
+        maxPage: maxPostPage(response),
+      };
+    });
+};
+
+const postsFromRange = (maxPage, url, from = true) => {
+  const request = [];
+  return new Promise((resolve) => {
+    for (var i = FIRST_ITERABLE_PAGE; i <= maxPage; i++) {
+      request.push(postsFromUrl(`${url}/${i}`, from));
+    }
+    Promise.all(request).then((result) => resolve(result.flat()));
+  });
+};
+
+const posts = (url, from = true) =>
+  firstPageDataFromUrl(url, from).then(({ firstPosts, maxPage }) =>
+    maxPage > 1
+      ? postsFromRange(maxPage, url, from).then((posts) => [
+          ...firstPosts,
+          ...posts,
+        ])
+      : firstPosts
+  );
+
+const userReceivedPosts = (user) => {
+  return posts(`https://www.postcrossing.com/user/${user}/gallery/received/`);
+};
+const userSentPosts = (user) => {
+  return posts(`https://www.postcrossing.com/user/${user}/gallery/sent`, false);
+};
+
+module.exports = { userReceivedPosts, userSentPosts };
