c46d245842fd024d1b9180c9293f7d5ab52eac11
Nicolás Delgado Abad <ndavaser@gmail.com>
2020-09-27 20:35:01 +0200

    more lint warnings, CI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import api from "./api";
 import SearchForm from "./components/searchForm";
