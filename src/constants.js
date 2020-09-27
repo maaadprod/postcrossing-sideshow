10284f522b4bb42e254166af596f1d381523587f
Nicolás Delgado Abad <ndavaser@gmail.com>
2020-09-27 20:18:26 +0200

    first environment

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const API_PROD = "/.netlify/functions";
 const API_DEV = "http://localhost:50064"
 
-export const api = API_DEV;
+export const api = process.env.REACT_APP_DEVENV === "development" ? API_DEV : API_PROD;
